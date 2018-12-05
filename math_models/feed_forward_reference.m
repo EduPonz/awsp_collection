15d3b0e3578321d07ba35afd425cccb4446e4426
IvelinPenchev <ivelin.penchev.95@gmail.com>
2018-12-05 08:15:59 +0100

    Updated force to pwm conversion

--- math_models/feed_forward_reference.m
+++ math_models/feed_forward_reference.m
@@ -1,11 +1,11 @@
  %% Dynamics calculator %%
  
  %% Initial and Goal parameters%%
- initial_position = [-1, 3]; 
- initial_bearing = pi/2;      % Initial angular position [rad]
+ initial_position = [0, 0]; 
+ initial_bearing = 0;      % Initial angular position [rad]
  initial_velocity = 0;
  
- goal_position  = [10,10];
+ goal_position  = [2,2];
  
  %% Boat Parameters %%
  
@@ -15,102 +15,177 @@ L1 = 0.61;                 % Distance from the motor shaft to x-axis [m]
 damping_yaw = 3.5;
 damping_surge = 2.7; 
 
+%% Time settings %%
+
+time_interval = 0.001;
+t = 0 : time_interval : 90;  % Discrete time vector
+time_points = length(t);    % Size of the time vector (total amount of time points)
+
 %% Forces and Torques %%
 
-ForceL = 1.38; % Force in left motor [N]
-ForceR = 1.4; % Force in right motor [N]
-input_torque = L1*ForceL - L1*ForceR; %% Torque that contributes to rotation
+ForceL = 2.7;                                         % Force in left motor [N]
+ForceR = 2;                                         % Force in right motor [N]
+input_torque = L1*ForceL - L1*ForceR;   % Torque that creates to rotation in yaw
 
 %% Velocities and Accelerations %%
 
-%syms position(t) cosine_angle(t) cartessian_position_x(t) cartessian_position_y(t);
-%syms linear_acc(t) linear_vel(t) linear_pos(t);
-%syms angular_acc(t) angular_vel(t) angular_pos(t);
-%syms xVelocity(t) yVelocity(t) xAcc(t) yAcc(t);
-t = 0 : 0.5 : 500;
-conv_rad_deg = 1; % Set to "1" if the desired unit is radians. Set to 180/pi if the desired unit is degrees
+linear_acc = zeros([1 time_points]);                        % generates a vector with the length of the time vector with all zeros
+linear_acc(1) = (ForceL + ForceR)/(mass);                  % sets the initial linear acceleration at the first entry of the linear acc vector
+linear_vel = zeros([1 time_points]);   % generates a vector with the length of the time vector with all zeros      
+linear_vel(1) = initial_velocity;
+linear_pos_displacement = zeros([1 time_points]);               % generates a vector with the length of the time vector with all zeros
 
-linear_acc = (ForceL + ForceR)./(mass + damping_surge.*t);
-linear_vel = initial_velocity + linear_acc.*t;
-linear_pos_displacement = 1/2*linear_acc.*t.*t;  
-
-angular_acc = conv_rad_deg.*input_torque./(inertia_z + damping_yaw.*t);
-angular_vel = angular_acc.*t;
-angular_pos = initial_bearing + 1/2*angular_acc.*t.*t;
+angular_acc = zeros([1 time_points]);           % generates a vector with the length of the time vector with all zeros
+angular_acc(1) = input_torque/(inertia_z);      % Initial angular acceleration
+angular_vel = zeros([1 time_points]);           % generates a vector with the length of the time vector with all zeros
+angular_pos = zeros([1 time_points]);           % generates a vector with the length of the time vector with all zeros
+angular_pos(1) = initial_bearing;           % Initial angular position (orientation of the boat in earth frame)
 
 
-cosine_angle = cos(initial_bearing + 1/2*angular_acc.*t.*t);
-sine_angle = sin(initial_bearing + 1/2*angular_acc.*t.*t);
+%% Main program %%
 
-xVelocity = (initial_velocity + linear_acc.*t) .* cosine_angle;
-yVelocity = (initial_velocity + linear_acc.*t) .* sine_angle;
+for k = 2 : time_points  % A loop that populates all acceleration, velocity and position vectors based on their corresponding formulas
+
+linear_acc(k) =(ForceL + ForceR - damping_surge*linear_vel(k-1))/(mass + damping_surge*time_interval);
+linear_vel(k) = linear_vel(k-1) + linear_acc(k-1)*time_interval;
+linear_pos_displacement(k) = linear_pos_displacement(k-1) + linear_vel(k-1)*time_interval + linear_acc(k-1)*time_interval^2;
+
+angular_acc(k) = (input_torque - damping_yaw*angular_vel(k-1))/(inertia_z + damping_yaw*time_interval);                    
+angular_vel(k) = angular_vel(k-1) + angular_acc(k-1)*time_interval;                         
+angular_pos(k) = angular_pos(k-1) + angular_vel(k-1)*time_interval + angular_acc(k-1)*time_interval^2;
+
+end
+
+
+%% Plotting the data onto a x-y cartessian plane %%
+
+cosine_angle = cos(angular_pos);
+sine_angle = sin(angular_pos);
+
+xVelocity = linear_vel .* cosine_angle;
+yVelocity = linear_vel .* sine_angle;
 
 xAcc = linear_acc .* cosine_angle;
 yAcc = linear_acc .* sine_angle;
 
-cartessian_position_x =  initial_position(1) + (1/2) * xAcc .* t.*t;
-cartessian_position_y =  initial_position(2) + (1/2) * yAcc .* t.*t;
+cartessian_position_x = zeros([1 time_points]);
+cartessian_position_x(1) =initial_position(1);
+cartessian_position_y = zeros([1 time_points]);
+cartessian_position_y(1) =initial_position(2);
 
-plot (cartessian_position_x, cartessian_position_y)
-xlim([-40 40])
-ylim([-40 40])
-
-
-
-
+x_goal = goal_position(1);
+y_goal = goal_position(2);
+%closest_angle = 0.1;
 
 
 
+for j = 2 : time_points
+
+cartessian_position_x(j) =  cartessian_position_x(j-1) + xVelocity(j-1) *time_interval + (1/2) * xAcc (j-1) * time_interval^2;
+cartessian_position_y(j) =  cartessian_position_y(j-1) + yVelocity(j-1) *time_interval + (1/2) * yAcc (j-1) * time_interval^2;
+
+angle_difference = atan2((y_goal - cartessian_position_y(j)), (x_goal - cartessian_position_x(j))) - angular_pos(j);
+
+% if abs(angle_difference) < 0.1
+%     if abs(angle_difference) < closest_angle
+%         closest_angle = angle_difference;
+%     end
+% end
+
+end
+
+constant_vel = linear_vel(90/0.001)
+
+linear_acc_for_const_vel =(ForceL + ForceR)/(mass + damping_surge*900000);
+linear_vel_constant= linear_acc_for_const_vel*900000
 
 
 
+desired_vel = 5;
+Forces_magnitude = (desired_vel*mass + desired_vel*damping_surge*500000)/500000
+
+constant_ang_vel = angular_vel(40/0.001);
+
+angular_acc_for_constant_ang_vel = (input_torque)/(inertia_z + damping_yaw*400000);                 
+angular_vel = angular_acc_for_constant_ang_vel*400000;                     
 
 
 
+%% Sample time (example time) %%
+
+% time_ex =4.95;                               % Time at which you want the example
+% t_ex = (time_interval^-1)*time_ex;          % Calculates in which entry of the time vector is the desired time for the example
+% angular_pos_ex = angular_pos(t_ex);
+% cartessian_position_x_ex = cartessian_position_x (t_ex);
+% cartessian_position_y_ex = cartessian_position_y (t_ex);
+% 
+% L = 50;
+% x2=cartessian_position_x_ex +(L*cos(angular_pos_ex));
+% y2=cartessian_position_y_ex +(L*sin(angular_pos_ex));
 
 
+
+%atan2((y_goal - cartessian_position_y_ex), (x_goal - cartessian_position_x_ex))*180/pi - angular_pos_ex*180/pi
 
 
 %% Figures Surge %%
 
-%figure(1)
-%fplot (linear_acc);
-%title('Linear Acceleration')
-%xlim([0 40])
-%ylim([0 10])
+% figure(1)
+% plot(t, linear_acc)
+% title('Linear Acc')
+% xlim([0 30])
+% ylim([0 10])
+% 
+ figure(2)
+ plot(t, linear_vel)
+ title('Linear Velocity')
+ xlim([0 30])
+ ylim([0 20])
+% 
+% figure(3)
+% plot(t, linear_pos_displacement)
+% title('Linear Position')
+% xlim([0 30])
+% ylim([0 100])
+% 
+% figure(4)
+% plot (cartessian_position_x, cartessian_position_y);
+% xlim([0 4])
+% ylim([0 4])
+% hold on;
+% %plot(cartessian_position_x_ex, cartessian_position_y_ex, 'r*');
+% %hold on;
+% plot(x_goal, y_goal, 'r*');
+%hold on;
+%plot([cartessian_position_x_ex x2],[cartessian_position_y_ex y2]);
 
-%figure(2)
-%fplot (linear_vel);
-%title('Linear Velocity')
-%xlim([0 40])
-%ylim([0 2])
-
-%figure(3)
-%fplot (position);
-%title('Linear Position')
-%xlim([0 20])
-%ylim([0 50])
-
-%% Figures Yaw %%
-
-
-%figure(4)
-%fplot (angular_acc);
-%title('Angular Acceleration')
-%xlim([0 20])
-%ylim([-50 50])
-
-%figure(5)
-%fplot (angular_vel);
-%title('Angular Velocity')
-%xlim([0 5])
-%ylim([0 1])
-
-%figure(6)
-%fplot (angular_pos);
-%title('Angular Position')
-%xlim([0 2])
-%ylim([-10 10])
+% 
+% %% Figures Yaw %%
+% 
+% 
+% figure(5)
+% plot(t, angular_acc)
+% title('Angular Acc')
+% xlim([0 30])
+% ylim([0 10])
+% 
+% figure(6)
+% plot(t, angular_vel)
+% title('angular vel')
+% xlim([0 30])
+% ylim([0 5])
+% 
+% figure(7)
+% plot(t, angular_pos)
+% title('Angular Position')
+% xlim([0 30])
+% ylim([0 10])
+% 
+% %figure(6)
+% %fplot (angular_pos);
+% %title('Angular Position')
+% %xlim([0 2])
+% %ylim([-10 10])
 
 
 
--- test_scripts/force_to_pwm/force_to_pwm.m
+++ test_scripts/force_to_pwm/force_to_pwm.m
@@ -1,20 +1,40 @@
 filedata = 'test_data.xlsx';
 
-increment_forward = xlsread(filedata,'A3:A11'); 
-increment_backward= xlsread(filedata,'F3:F11'); 
+increment_increase = xlsread(filedata,'A3:A11'); 
+increment_decrease= xlsread(filedata,'F3:F11'); 
 
 force_forward_left = xlsread(filedata,'B3:B11');
-force_forward_right = xlsread(filedata,'C3:C11');
+force_forward_right = xlsread(filedata,'H3:H11');
 
 force_backward_left = xlsread(filedata,'G3:G11');
-force_backward_right = xlsread(filedata,'H3:H11');
+force_backward_right = xlsread(filedata,'C3:C11');
 
-fit_forward_left = fit(force_forward_left, increment_forward, 'poly2')
-fit_forward_right = fit(force_forward_right,increment_forward, 'poly2')
+fit_forward_left = fit(force_forward_left, increment_increase, 'poly1')
+fit_forward_right = fit(force_forward_right,increment_decrease, 'poly1')
 
-fit_backward_left = fit(force_backward_left,increment_backward, 'poly2')
-fit_backward_right = fit(force_backward_right,increment_backward,'poly2')
+fit_backward_left = fit(force_backward_left,increment_decrease, 'poly1')
+fit_backward_right = fit(force_backward_right,increment_increase,'poly1')
 
-plot(force_backward_left, increment_backward)
+figure(1)
+plot(force_forward_left, increment_increase, '*')
+title('Forward Left')
+hold on;
+plot(fit_forward_left);
+
+figure(2)
+plot(force_forward_right, increment_decrease, '*')
+title('Forward Right')
+hold on;
+plot(fit_forward_right);
+
+figure(3)
+plot(force_backward_left, increment_decrease, '*')
+title('Backward Left')
 hold on;
 plot(fit_backward_left);
+
+figure(4)
+plot(force_backward_right, increment_increase, '*')
+title('Backward Right')
+hold on;
+plot(fit_backward_right);
